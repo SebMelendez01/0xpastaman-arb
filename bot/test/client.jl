35fbc7f7939f4fbcf7ba6a1e7132e34953388e0c
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2023-08-07 23:05:45 -0600

    Delete bot/test directory

--- bot/test/client.jl
+++
@@ -1,9 +0,0 @@
-using Sockets
-
-clientside = connect(8080)
-
-println(clientside,"Hello World from the Echo Server")
-line = readline(clientside)
-print(line);
-
-close(clientside)
--- bot/test/index.js
+++
@@ -1,9 +0,0 @@
-// import net from "net"
-const net = require("net");
-
-
-const server = net.createServer((socket) => {
-    socket.end(`${new Date()}\n`)
-  })
-  
-  server.listen(59090)
